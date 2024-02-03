d0c35ecc6e3ee0a19feb01160cc80528ea54462c
unknown <chohankoti333@gmail.com>
2024-02-03 21:37:05 +0530

    at the end

--- src/components/Contack.jsx
+++ src/components/Contack.jsx
@@ -49,9 +49,9 @@ export default function Contact() {
         </div>
       </form>
 
-      <div className="flex flex-col">
-        <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
-          <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+      <div className="flex flex-col overflow-x-auto">
+        <div className="-my-2 sm:-mx-6 lg:-mx-8">
+          <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8 ">
             <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
               {data.length > 0 && (
                 <table className="min-w-full divide-y divide-gray-200">
@@ -73,19 +73,15 @@ export default function Contact() {
                       <tr key={index}>
                         {Object.keys(row).map((key) => (
                           <td key={key} className="px-6 py-4 whitespace-nowrap">
-                            <div className="flex items-center">
-                              {key === 'hotel_image' ? (
-                                <div className="flex-shrink-0 h-10 w-10">
-                                  <img className="h-10 w-10 rounded-full" src={row[key]} alt="" />
-                                </div>
-                              ) : (
-                               {key !== 'VERIFIED_STATUS' ? (
-                                <div className="ml-4">
+                            {key === 'hotel_image' ? (
+                              <div className="flex-shrink-0 h-10 w-10">
+                                <img className="h-10 w-10 rounded-full" src={row[key]} alt="" />
+                              </div>
+                            ) : (
+                              <div className="ml-4">
                                 <div className="text-sm font-medium text-gray-900">{row[key]}</div>
-                                </div>
-                              ):()}                               
-                              )}
-                            </div>
+                              </div>
+                            )}
                           </td>
                         ))}
                       </tr>
