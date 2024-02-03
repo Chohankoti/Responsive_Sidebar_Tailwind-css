836fda8372e0df538bc826560d2eebf140ca56e5
unknown <chohankoti333@gmail.com>
2024-02-03 15:48:02 +0530

    excel reading and displaying data in table done but styling left

--- src/components/Contack.jsx
+++ src/components/Contack.jsx
@@ -1,7 +1,47 @@
-import React from 'react'
+import React, { useState } from 'react';
+import * as XLSX from 'xlsx';
 
-export default function Contack() {
+export default function Contact() {
+  const [data, setData] = useState([]);
+
+  const handleFileUpload = (e) => {
+    const reader = new FileReader();
+    reader.readAsArrayBuffer(e.target.files[0]); // Change to readAsArrayBuffer
+    reader.onload = (e) => {
+      const arrayBuffer = e.target.result;
+      const data = new Uint8Array(arrayBuffer);
+      const workbook = XLSX.read(data, { type: 'array' });
+      const sheetName = workbook.SheetNames[0];
+      const sheet = workbook.Sheets[sheetName];
+      const parsedData = XLSX.utils.sheet_to_json(sheet);
+      setData(parsedData);
+    };
+  };
+
   return (
-    <div>Contack</div>
-  )
+    <>
+      <input type='file' accept='.xlsx, .xls' onChange={handleFileUpload} />
+
+      {data.length > 0 && (
+        <table>
+          <thead>
+            <tr>
+              {Object.keys(data[0]).map((key) => (
+                <th key={key}>{key}</th>
+              ))}
+            </tr>
+          </thead>
+          <tbody>
+            {data.map((row, index) => (
+              <tr key={index}>
+                {Object.values(row).map((value, index) => (
+                  <td key={index}>{value}</td>
+                ))}
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
+    </>
+  );
 }
