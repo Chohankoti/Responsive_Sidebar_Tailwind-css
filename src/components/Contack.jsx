02f3daf153412c2959b5da92d245570869223620
unknown <chohankoti333@gmail.com>
2024-02-03 16:08:55 +0530

    input style done

--- src/components/Contack.jsx
+++ src/components/Contack.jsx
@@ -1,27 +1,54 @@
 import React, { useState } from 'react';
 import * as XLSX from 'xlsx';
+import { IoCloudUploadOutline } from 'react-icons/io5';
 
 export default function Contact() {
   const [data, setData] = useState([]);
+  const [selectedFile, setSelectedFile] = useState("No file chosen");
 
   const handleFileUpload = (e) => {
-    const reader = new FileReader();
-    reader.readAsArrayBuffer(e.target.files[0]);
-    reader.onload = (e) => {
-      const arrayBuffer = e.target.result;
-      const data = new Uint8Array(arrayBuffer);
-      const workbook = XLSX.read(data, { type: 'array' });
-      const sheetName = workbook.SheetNames[0];
-      const sheet = workbook.Sheets[sheetName];
-      const parsedData = XLSX.utils.sheet_to_json(sheet);
-      setData(parsedData);
-    };
+    const file = e.target.files[0];
+
+    if (file) {
+      setSelectedFile(file.name);
+
+      const reader = new FileReader();
+      reader.readAsArrayBuffer(file);
+      reader.onload = (e) => {
+        const arrayBuffer = e.target.result;
+        const data = new Uint8Array(arrayBuffer);
+        const workbook = XLSX.read(data, { type: 'array' });
+        const sheetName = workbook.SheetNames[0];
+        const sheet = workbook.Sheets[sheetName];
+        const parsedData = XLSX.utils.sheet_to_json(sheet);
+        setData(parsedData);
+      };
+    }
   };
 
   return (
-    <>
-      <input type='file' accept='.xlsx, .xls' onChange={handleFileUpload} />
-
+    <div className="m-4">
+      <form className='m-3'>
+        <div className="flex flex-row items-center">
+          <input
+            type="file"
+            id="custom-input"
+            accept=".xlsx, .xls"
+            onChange={handleFileUpload}
+            hidden
+          />
+          <label
+            htmlFor="custom-input"
+            className="block text-sm text-slate-500 mr-4 py-2 px-4
+              rounded-md border-0 text-sm font-semibold bg-pink-50
+              text-pink-700 hover:bg-pink-100 cursor-pointer"
+          >
+           <IoCloudUploadOutline className="w-6 h-6" />
+          </label>
+          <label className="text-sm text-slate-500">{selectedFile}</label>
+        </div>
+      </form>
+    
       {data.length > 0 && (
         <table>
           <thead>
@@ -48,6 +75,6 @@ export default function Contact() {
           </tbody>
         </table>
       )}
-    </>
+    </div>
   );
 }
